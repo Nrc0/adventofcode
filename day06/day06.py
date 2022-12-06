1aa36d5d5eec3976c21bf155e09a89a49b1d68db
Corentin QUESTE <queste.corentin@gmail.com>
2022-12-06 09:45:59 +0100

    Update day06.py

--- day06/day06.py
+++ day06/day06.py
@@ -3,17 +3,16 @@ path='input\\day06.txt'
 with open(path) as f:
     data = f.read().strip()
 
-p = data
 i = 0; j = 0
 
 # part 1
-while len(set(p[:4])) != 4:
-    p = p[1:]
+while len(set(data[:4])) != 4:
+    data = data[1:]
     i += 1
 
 #part 2
-while len(set(p[:14])) != 14:
-    p = p[1:]
+while len(set(data[:14])) != 14:
+    data = data[1:]
     j += 1
 
 print(i+4)
