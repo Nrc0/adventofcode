8b1de78bcf6d72ef83cd8a9a73a55ad9be0287bd
Corentin QUESTE <queste.corentin@gmail.com>
2022-12-11 16:52:11 +0100

    Add files via upload

---
+++ day10/day10.py
@@ -0,0 +1,19 @@
+input_path= 'C:\\Users\\quest\\OneDrive\\Documents\\adventofcode2022\\day10\\day10.txt'
+
+with open(input_path) as f:
+    ls = f.read().strip().split("\n")
+
+xs = [1]
+for l in ls:
+    x = xs[-1]
+    xs.append(x)
+    if a := l[5:]:
+        xs.append(x + int(a))
+
+# Part 1
+print("p1: ",sum(i * xs[i - 1] for i in (20, 60, 100, 140, 180, 220)))
+
+# Part 2
+print("p2: ")
+for i, x in enumerate(xs):
+    print("█" if abs(i % 40 - x) <= 1 else " ", end="" if (i + 1) % 40 else "\n")
